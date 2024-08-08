8acaf280738b4ad1904f1706a5333dfcc1195619
YiyanZhai <zhaiyiyan17@gmail.com>
2024-08-08 19:04:43 -0400

    improving fallback

--- python/mlc_llm/op/attention.py
+++ python/mlc_llm/op/attention.py
@@ -1,12 +1,15 @@
 """Operators enabled by external modules."""
 
 import math
-
+import tvm
 from tvm import tir
+from tvm.script import tir as T
 from tvm.relax.frontend import nn
 from tvm.relax.frontend.nn import op
 
 from mlc_llm.support import logging
+
+from mlc_llm.nn.kv_cache import _attention_prefill_ragged
 
 from . import extern as _extern
 
@@ -57,7 +60,7 @@ def attention(  # pylint: disable=invalid-name,too-many-locals,too-many-statemen
 
     --- Other params ---
     qk_dtype: if set, `matmul(Q, K, out_dtype=qk_dtype)`, (otherwise use `q.dtype` as `out_dtype`).
-        For FlashInfer, if "float32", sets `allow_fp16_qk_reduction` to False; otherwise no effect.
+        For FlashInfer, if "float32", sets `allow_fp16_qk_reduction` to False; otherwise no effectir.
     """
     assert q.ndim == 4 and k.ndim in [3, 4] and v.ndim in [3, 4]
     b, s, h_q, d = q.shape
@@ -66,34 +69,69 @@ def attention(  # pylint: disable=invalid-name,too-many-locals,too-many-statemen
 
     def _fallback():
         nonlocal q, k, v, qk_dtype
-        if k.ndim == 3:
-            k = op.reshape(k, [b, t, h_kv, d])
-        if v.ndim == 3:
-            v = op.reshape(v, [b, t, h_kv, d])
         if h_kv != h_q:
             k = k.repeat(h_q // h_kv, axis=2)
             v = v.repeat(h_q // h_kv, axis=2)
-        q = op.permute_dims(q, [0, 2, 1, 3])
-        k = op.permute_dims(k, [0, 2, 1, 3])
-        v = op.permute_dims(v, [0, 2, 1, 3])
-        model_dtype = q.dtype
-        if qk_dtype is None:
-            qk_dtype = model_dtype
-        attn_weights = op.matmul(  # [b, h, s, t]
-            q,  # [b, h, s, d]
-            op.permute_dims(k, [0, 1, 3, 2]),  # [b, h, d, t]
-            out_dtype=qk_dtype,
-        ) / math.sqrt(d)
-        if attn_score_scaling_factor != 1.0:
-            attn_weights = attn_weights * attn_score_scaling_factor
-        attn_weights = attn_weights.maximum(tir.min_value(model_dtype)).minimum(
-            casual_mask.astype(qk_dtype)
+        q = op.reshape(q, [b*s, h_q, d])
+        k = op.reshape(k, [b*s, h_q, d])
+        v = op.reshape(v, [b*s, h_q, d])
+        q_rope_position = op.zeros([b*s], dtype="float16")
+        k_rope_pos_offset = op.zeros([1], dtype="float16")
+        # output = tvm.nd.empty((b*s, h_q, d))
+        # lse = tvm.nd.empty((b*s, h_q))
+        causal = T.int32(1)
+        if casual_mask is None:
+            causal = T.int32(0)
+        rotary_mode = T.int32(0)
+        rope_scale = T.float32(1.0)
+        rope_theta = T.float32(0.0)
+        attn_score_scaling_factor = T.float32(1.0)
+        # print the type of each variable nn.Tensor.placeholder((b*s, h_q, d), "float32"), nn.Tensor.placeholder((b*s, h_q), "float32")
+        result = op.tensor_ir_op(
+            _attention_prefill_ragged(h_q, h_q, d, q.dtype, [], tvm.target.Target("cuda")),
+            # batch_prefill_ragged_kv,
+            name_hint="batch_prefill_ragged_kv",
+            args=[
+                q, k, v,
+                q_rope_position, k_rope_pos_offset, 
+                # output, 
+                # lse, 
+                causal, rotary_mode,
+                rope_scale, rope_theta,
+                attn_score_scaling_factor
+            ],
+            out = [nn.Tensor.placeholder((b*s, h_q, d), "float32"), nn.Tensor.placeholder((b*s, h_q), "float32")],
+            # out=[nn.Tensor.placeholder(q.shape, q.dtype), nn.Tensor.placeholder((q.shape[0], q.shape[1]), "float32")],
         )
-        attn_weights = op.softmax(attn_weights.astype("float32"), axis=-1).astype(model_dtype)
-        output = op.matmul(attn_weights, v)  # [b, h, s, d] <= [b, h, s, t] x [b, h, t, d]
-        output = op.permute_dims(output, [0, 2, 1, 3])  #  [b, s, h, d]
-        output = op.reshape(output, [b, s, h_q * d])  # [b, s, h * d]
-        return output
+        return result[0]
+        # if k.ndim == 3:
+        #     k = op.reshape(k, [b, t, h_kv, d])
+        # if v.ndim == 3:
+        #     v = op.reshape(v, [b, t, h_kv, d])
+        # if h_kv != h_q:
+        #     k = k.repeat(h_q // h_kv, axis=2)
+        #     v = v.repeat(h_q // h_kv, axis=2)
+        # q = op.permute_dims(q, [0, 2, 1, 3])
+        # k = op.permute_dims(k, [0, 2, 1, 3])
+        # v = op.permute_dims(v, [0, 2, 1, 3])
+        # model_dtype = q.dtype
+        # if qk_dtype is None:
+        #     qk_dtype = model_dtype
+        # attn_weights = op.matmul(  # [b, h, s, t]
+        #     q,  # [b, h, s, d]
+        #     op.permute_dims(k, [0, 1, 3, 2]),  # [b, h, d, t]
+        #     out_dtype=qk_dtype,
+        # ) / math.sqrt(d)
+        # if attn_score_scaling_factor != 1.0:
+        #     attn_weights = attn_weights * attn_score_scaling_factor
+        # attn_weights = attn_weights.maximum(tir.min_value(model_dtype)).minimum(
+        #     casual_mask.astype(qk_dtype)
+        # )
+        # attn_weights = op.softmax(attn_weights.astype("float32"), axis=-1).astype(model_dtype)
+        # output = op.matmul(attn_weights, v)  # [b, h, s, d] <= [b, h, s, t] x [b, h, t, d]
+        # output = op.permute_dims(output, [0, 2, 1, 3])  #  [b, s, h, d]
+        # output = op.reshape(output, [b, s, h_q * d])  # [b, s, h * d]
+        # return output
 
     # FlashInfer Implementation
     if (
