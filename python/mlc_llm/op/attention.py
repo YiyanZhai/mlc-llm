cadee5a6ae6e94a03ed039c82da26a4a6c879148
YiyanZhai <zhaiyiyan17@gmail.com>
2024-08-12 12:20:50 -0400

    attn tir impl without floats

--- python/mlc_llm/op/attention.py
+++ python/mlc_llm/op/attention.py
@@ -1,15 +1,10 @@
 """Operators enabled by external modules."""
 
-import math
 import tvm
-from tvm import tir
-from tvm.script import tir as T
 from tvm.relax.frontend import nn
-from tvm.relax.frontend.nn import op
+from tvm.relax.frontend.nn import Tensor, op
 
 from mlc_llm.support import logging
-
-from mlc_llm.nn.kv_cache import _attention_prefill_ragged
 
 from . import extern as _extern
 
@@ -60,7 +55,7 @@ def attention(  # pylint: disable=invalid-name,too-many-locals,too-many-statemen
 
     --- Other params ---
     qk_dtype: if set, `matmul(Q, K, out_dtype=qk_dtype)`, (otherwise use `q.dtype` as `out_dtype`).
-        For FlashInfer, if "float32", sets `allow_fp16_qk_reduction` to False; otherwise no effectir.
+        For FlashInfer, if "float32", sets `allow_fp16_qk_reduction` to False; otherwise no effect.
     """
     assert q.ndim == 4 and k.ndim in [3, 4] and v.ndim in [3, 4]
     b, s, h_q, d = q.shape
@@ -68,48 +63,31 @@ def attention(  # pylint: disable=invalid-name,too-many-locals,too-many-statemen
     group_size = h_q // h_kv
 
     def _fallback():
-        from tvm import relax as rx
+        from mlc_llm.nn import _attention_sequence_prefill # pylint: disable=import-outside-toplevel, cyclic-import
+
         nonlocal q, k, v, qk_dtype
+        if k.ndim == 3:
+            k = op.reshape(k, [b, t, h_kv, d])
+        if v.ndim == 3:
+            v = op.reshape(v, [b, t, h_kv, d])
         if h_kv != h_q:
             k = k.repeat(h_q // h_kv, axis=2)
             v = v.repeat(h_q // h_kv, axis=2)
-        q = op.reshape(q, [b*s, h_q, d])
-        k = op.reshape(k, [b*s, h_q, d])
-        v = op.reshape(v, [b*s, h_q, d])
-        q_rope_position = op.zeros([b*s], dtype="float16")
-        k_rope_pos_offset = op.zeros([1], dtype="float16")
-        causal = T.int32(1)
-        if casual_mask is None:
-            causal = T.int32(0)
-        rotary_mode = T.int32(0)
-        rope_scale = T.float32(1.0)
-        rope_theta = T.float32(0.0)
-        attn_score_scaling_factor = T.float32(1.0)
-        o = op.empty([b*s, h_q, d], dtype="float32")
-        lse = op.empty([b*s, h_q], dtype="float32")
-        func = rx.BlockBuilder().current().add_func(_attention_prefill_ragged(h_q, h_q, d, q.dtype, [], tvm.target.Target("cuda")), func_name="batch_prefill_ragged_kv")
-        rx.op.call_pure_packed("batch_prefill_ragged_kv", func,
-                q._expr, k._expr, v._expr,
-                q_rope_position._expr, k_rope_pos_offset._expr,
-                causal, rotary_mode,
-                rope_scale, rope_theta,
-                attn_score_scaling_factor,
-                o._expr, lse._expr,
-                sinfo_args=rx.ObjectStructInfo())
-        # result = op.tensor_ir_op(
-        #     _attention_prefill_ragged(h_q, h_q, d, q.dtype, [], tvm.target.Target("cuda")),
-        #     name_hint="batch_prefill_ragged_kv",
-        #     args=[
-        #         q, k, v,
-        #         q_rope_position, k_rope_pos_offset,
-        #         causal, rotary_mode,
-        #         rope_scale, rope_theta,
-        #         attn_score_scaling_factor
-        #     ],
-        #     out = [nn.Tensor.placeholder((b*s), (h_q), (d), "float32"), nn.Tensor.placeholder((T.int64(b*s), T.int64(h_q)), "float32")],
-        #     # out=[nn.Tensor.placeholder(q.shape, q.dtype), nn.Tensor.placeholder((q.shape[0], q.shape[1]), "float32")],
-        # )
-        return op.wrap_nested(o, "attn_output")
+
+        target = tvm.target.Target("cuda")
+        attn_output, _ = op.tensor_ir_op(
+            _attention_sequence_prefill(h_kv, h_q, d, q.dtype, target),
+            "sequence_prefill",
+            [q, k, v],
+            [
+                Tensor.placeholder([b, s, h_q, d], q.dtype),
+                Tensor.placeholder([b, s, h_q], q.dtype),
+            ],
+        )
+
+        attn_output = op.reshape(attn_output, shape=(b, s, h_q, d))
+        output = op.reshape(attn_output, shape=(b, s, h_q * d))
+        return output
 
     # FlashInfer Implementation
     if (
